f4b27bde120b667ed5cc14e679d7020fecbc4002
Vadim Roziznan <roziznan_v@mail.ru>
2023-05-12 19:28:04 +0300

    12.05.23

--- 2.2-databases-2/orm_migrations/website/urls.py
+++ 2.2-databases-2/orm_migrations/website/urls.py
@@ -21,4 +21,11 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('school.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
