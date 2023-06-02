d8eecd7910a71c0a396052e350f27a1376719fcb
Vadim Roziznan <roziznan_v@mail.ru>
2023-06-02 18:38:09 +0300

    work_in_progress

--- 3.2-crud/stocks_products/logistic/models.py
+++ 3.2-crud/stocks_products/logistic/models.py
@@ -6,6 +6,9 @@ class Product(models.Model):
     title = models.CharField(max_length=60, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return f'id: {self.id}, title: {self.title}, description: {self.description}'
+
 
 class Stock(models.Model):
     address = models.CharField(max_length=200, unique=True)
@@ -15,6 +18,8 @@ class Stock(models.Model):
         related_name='stocks',
     )
 
+    def __str__(self):
+        return f'address: {self.address}, products: {self.products}'
 
 class StockProduct(models.Model):
     stock = models.ForeignKey(
--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,27 +1,49 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from logistic. models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+
+    title = serializers.CharField(max_length=60)
+
+    class Meta:
+
+        model = Product
+        fields = '__all__'
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+
+    class Meta:
+
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+
+        model = Stock
+        fields = '__all__'
 
     def create(self, validated_data):
+        # print(validated_data)
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
+        # for position in positions:
+        #     print(position.pop('price'))
+        # print(positions)
         # создаем склад по его параметрам
+
         stock = super().create(validated_data)
+        # print(stock)
+        for position in positions:
+            # print(position.pop('product').id)
+            s = StockProduct.objects.create(stock_id=stock.id, product_id=position.pop('product').id, price=position.pop('price'), quantity=position.pop('quantity'))
+            s.save()
 
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
