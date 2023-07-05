f906ca81c3e479e0f40a85265ed04b72022b511e
RoziznanVadim <roziznan_v@mail.ru>
2023-07-05 18:40:48 +0300

    h_w

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,3 +1,4 @@
+
 import pytest
 
 from django.contrib.auth.models import User
@@ -36,24 +37,108 @@ def student_factory():
 def test_get_course(client, user, course_factory):
     #Arrange
     course = course_factory()
-    # course = course_factory(_quantity=5)
 
     #Act
-    response = client.get("/api/v1/courses/")
+    response = client.get('/api/v1/courses/')
 
     #Assert
     assert response.status_code == 200
     data = response.json()
-    assert
-    # assert len(data) == len(course)
-    # for i, m in enumerate(data):
-    #     assert m["name"] == course[i].name
+    assert data[0]['name'] == course.name
 
 
-# @pytest.mark.django_db
-# def test_create_course(client):
-#     count = Course.objects.count()
-#     response = client.post("/courses/",
-#                            data={"name": })
+@pytest.mark.django_db
+def test_get_courses(client, user, course_factory):
+    #Arrange
+    courses = course_factory(_quantity=10)
+
+    #Act
+    response = client.get('/api/v1/courses/')
+
+    #Assert
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == len(courses)
+    for i, m, in enumerate(data):
+        assert m['name'] == courses[i].name
+
+
+@pytest.mark.django_db
+def test_check_id(client, user, course_factory):
+    # Arrange
+    courses = course_factory(_quantity=10)
+
+    # Act
+    response = client.get(f'/api/v1/courses/?id={courses[2].id}')
+
+    # Assert
+    data = response.json()
+    assert response.status_code == 200
+    assert data[0]['id'] == courses[2].id
+
+
+@pytest.mark.django_db
+def test_check_name(client, user, course_factory):
+    # Arrange
+    courses = course_factory(_quantity=10)
+
+    # Act
+    response = client.get(f'/api/v1/courses/?name={courses[5].name}')
+
+    # Assert
+    data = response.json()
+    assert response.status_code == 200
+    assert data[0]['name'] == courses[5].name
+
+
+@pytest.mark.django_db
+def test_post_course(client, user):
+    # Arrange
+    count = Course.objects.count()
+
+    # Act
+    response = client.post(
+        '/api/v1/courses/',
+        data={
+            'name': 'C',
+            'student_id': user.id
+        })
+
+    # Assert
+    assert response.status_code == 201
+    assert Course.objects.count() == count + 1
+
+
+@pytest.mark.django_db
+def test_patch_course(client, user, course_factory):
+    # Arrange
+    course = course_factory()
+
+    # Act
+    json_data = {
+        "name": "C"
+    }
+    response = client.patch(f"/api/v1/courses/{course.id}/", data=json_data)
+
+    # Assert
+    assert response.status_code == 200
+    course.refresh_from_db()
+    assert course.name == "C"
+
+
+@pytest.mark.django_db
+def test_delete_course(client, user, course_factory):
+    # Arrange
+    course = course_factory()
+
+    # Act
+    response = client.delete(f"/api/v1/courses/{course.id}/")
+
+    # Assert
+    assert response.status_code == 204  # Проверка успешного кода состояния
+
+    # Убедитесь, что курс был удален из базы данных
+    with pytest.raises(Course.DoesNotExist):
+        course.refresh_from_db()
 
 
